e6ee48b99e83c79b6486015f561ab76712544dac
Austen Lane-Roberts <austen.lane.roberts@gmail.com>
2017-11-02 13:48:00 -0700

    -update app structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
-import SubredditsPage from './containers/SubredditsPage';
-import PostsPage from './containers/PostsPage';
+import store from '../store';
+import SubredditsPage from './SubredditsPage';
+import PostsPage from './PostsPage';
 
 const FourOhFour = () => <h1>404</h1>;
 
